890c88841e86e015c2c05a4b73308191303f7fc6
Семён Жуков <skrilleks2002777@gmail.com>
2020-10-13 11:06:35 +0500

    MenuConroller, Animation button, Canvas Scale

---
+++ Assets/scripts/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void Play(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void Score()
+    {
+
+    }
+    public void Settings()
+    {
+
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
