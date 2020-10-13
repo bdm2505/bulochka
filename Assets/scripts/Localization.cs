b6ba2bb33d2e6b4a33e11ffaacfca697ee610711
bdm <bdm1999@yandex.ru>
2020-10-13 16:06:47 +0500

    my translate

---
+++ Assets/scripts/Localization.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Localization : MonoBehaviour
+{
+
+    private void Start()
+    {
+        GetComponent<Text>().text = Translator.Translate(GetComponent<Text>().text);
+    }
+}
---
+++ Assets/scripts/Translator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Translator 
+{
+    public const int EN = 1;
+    public const int RU = 0;
+    public static int LANG = RU;
+
+    private static Dictionary<string, string> en = new Dictionary<string, string>()
+    {
+        {"play", "play"},
+        {"setting", "setting"}
+    };
+    private static Dictionary<string, string> ru = new Dictionary<string, string>()
+    {
+        {"play", "играть"},
+        {"setting", "настройки"}
+    };
+
+    public static string Translate(string s)
+    {
+        var dic = LANG == RU ? ru : en;
+        if (dic.ContainsKey(s))
+            return dic[s];
+        
+        Debug.Log("no translate for '" + s + "'");
+        return s;
+    }
+}
