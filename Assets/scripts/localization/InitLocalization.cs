735f9b64462b714c2f3ade6be998cafb6022f433
bdm <bdm1999@yandex.ru>
2020-10-13 17:10:32 +0500

    add translate

---
+++ Assets/scripts/localization/InitLocalization.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+namespace localization
+{
+	/// <summary>
+	/// Asset usage example.
+	/// </summary>
+	public class InitLocalization : MonoBehaviour
+	{
+		public void Awake()
+		{
+			LocalizationManager.Read();
+
+			switch (Application.systemLanguage)
+			{
+				case SystemLanguage.Russian:
+					LocalizationManager.Language = "Russian";
+					break;
+				default:
+					LocalizationManager.Language = "English";
+					break;
+			}
+		}
+	}
+}
