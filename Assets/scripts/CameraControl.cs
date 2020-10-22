f6e296df909fbdb1423dcbfb729d8dcbbff4247a
bdm <bdm1999@yandex.ru>
2020-10-22 16:44:11 +0500

    a

--- Assets/scripts/CameraControl.cs
+++ Assets/scripts/CameraControl.cs
@@ -8,11 +8,20 @@ public class CameraControl : MonoBehaviour
 	private Vector2 offset = new Vector2(0, 0);
 	public Transform player;
 	private BaseCharacter character;
+	public BoxCollider2D container;
+	private float min_x;
+	private float min_y;
+	private float max_x;
+	private float max_y;
 
 	void Start()
 	{
 		if (player == null) player = GameObject.FindGameObjectWithTag("Player").transform;
 		character = player.GetComponent<BaseCharacter>();
+		min_x = container.bounds.min.x;
+		min_y = container.bounds.min.y;
+		max_x = container.bounds.max.x;
+		max_y = container.bounds.max.y;
 	}
 
 	void FixedUpdate()
@@ -22,6 +31,7 @@ public class CameraControl : MonoBehaviour
 			offset = character.CalculateMoveVector() * 5;
 
 			var target = new Vector3(player.position.x + offset.x, player.position.y + offset.y, transform.position.z);
+			
 
 			Vector3 currentPosition = Vector3.Lerp(transform.position, target, damping * Time.deltaTime);
 			transform.position = currentPosition;
